111cfb67d48db65a06e51b8836a54d908ca4a814
cjhsu1991 <dd86042@gmail.com>
2017-08-28 13:43:08 +0800

    add_custom_template

--- mySite/trips/views.py
+++ mySite/trips/views.py
@@ -2,6 +2,8 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from datetime import datetime
 
 def hello(request):
-    return HttpResponse("Hello World!")
+    return render(request, 'hello_world.html', {'current_time':str(datetime.now())})
+	#return HttpResponse("Hello World!")
